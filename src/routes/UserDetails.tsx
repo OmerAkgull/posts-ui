fac0f644de9efbaae6fbf4dacdc091fd3d20b7af
OmerAkgull <143122832+OmerAkgull@users.noreply.github.com>
2024-02-22 17:51:09 +0300

    Create tablist

--- src/routes/UserDetails.tsx
+++ src/routes/UserDetails.tsx
@@ -1,5 +1,6 @@
 import { useLoaderData } from "react-router-dom";
 import { Box, Text, Card, CardHeader, CardBody } from "@chakra-ui/react";
+import { Tabs, TabList, TabPanels, Tab, TabPanel } from "@chakra-ui/react";
 import { Params } from "react-router-dom";
 
 interface User {
@@ -26,17 +27,17 @@ interface User {
   };
 }
 export async function loader({ params }: { params?: Params }) {
-    if (!params || !params.userId) {
-      throw new Error("Invalid params or userId");
-    }
-  
-    const response = await fetch(
-      `https://jsonplaceholder.typicode.com/users/${params.userId}`
-    );
-    const user = (await response.json()) as User;
-  
-    return { user };
+  if (!params || !params.userId) {
+    throw new Error("Invalid params or userId");
   }
+
+  const response = await fetch(
+    `https://jsonplaceholder.typicode.com/users/${params.userId}`
+  );
+  const user = (await response.json()) as User;
+
+  return { user };
+}
 
 export default function UserDetails() {
   const { user } = useLoaderData() as Awaited<ReturnType<typeof loader>>;
@@ -46,24 +47,55 @@ export default function UserDetails() {
   }
 
   return (
-    <Box
-      display="flex"
-      flexDir="column"
-      justifyContent="center"
-      alignItems="center"
-      py={10}
-    >
-      <Card align="center" justify="center" colorScheme="green" my={5}>
-        <CardHeader fontWeight="medium" fontSize={{ base: "16", md: "24" }}>
-          {user.name}
-        </CardHeader>
-        <CardBody>
-          <Text>Username: {user.username}</Text>
-          <Text>Email: {user.email}</Text>
-          <Text>Phone: {user.phone}</Text>
-          <Text>Website: {user.website}</Text>
-        </CardBody>
-      </Card>
-    </Box>
+    <Tabs align="center">
+      <TabList>
+        <Tab>User</Tab>
+        <Tab>Posts</Tab>
+        <Tab>Albums</Tab>
+        <Tab>Todos</Tab>
+      </TabList>
+
+      <TabPanels>
+        <TabPanel>
+          <Box
+            display="flex"
+            flexDir="column"
+            justifyContent="center"
+            alignItems="center"
+            py={10}
+          >
+            <Card
+              size="lg"
+              align="center"
+              justify="center"
+              colorScheme="green"
+              my={5}
+            >
+              <CardHeader
+                fontWeight="medium"
+                fontSize={{ base: "16", md: "24" }}
+              >
+                {user.name}
+              </CardHeader>
+              <CardBody>
+                <Text>Username: {user.username}</Text>
+                <Text>Email: {user.email}</Text>
+                <Text>Phone: {user.phone}</Text>
+                <Text>Website: {user.website}</Text>
+              </CardBody>
+            </Card>
+          </Box>
+        </TabPanel>
+        <TabPanel>
+          <p>two!</p>
+        </TabPanel>
+        <TabPanel>
+          <p>three!</p>
+        </TabPanel>
+        <TabPanel>
+          <p>four!</p>
+        </TabPanel>
+      </TabPanels>
+    </Tabs>
   );
 }
