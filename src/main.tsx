92faf2c1ee78bcd9f6cdc0a3b0f85aa8af7832fa
OmerAkgull <143122832+OmerAkgull@users.noreply.github.com>
2024-02-22 13:28:39 +0300

    Create Nav and Footer

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { ChakraProvider } from "@chakra-ui/react";
+import Layout from "./routes/layout";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout></Layout>,
+  },
+]);
 
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-
-  </React.StrictMode>,
-)
+    <ChakraProvider>
+    <RouterProvider router={router}></RouterProvider>
+    </ChakraProvider>
+  </React.StrictMode>
+);
---
+++ src/routes/layout.tsx
@@ -0,0 +1,11 @@
+import Footer from "../components/layout/Footer";
+import NavBar from "../components/layout/Navbar";
+
+export default function Layout() {
+return (
+    <>
+    <NavBar></NavBar>
+    <Footer></Footer>
+    </>
+)
+}
