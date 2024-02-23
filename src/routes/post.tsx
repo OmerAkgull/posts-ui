a0db5e813216458caace17bef0b3c3fccb5f9a4c
OmerAkgull <143122832+OmerAkgull@users.noreply.github.com>
2024-02-23 12:48:42 +0300

    Map over comments

--- src/routes/post.tsx
+++ src/routes/post.tsx
@@ -9,6 +9,14 @@ interface Post {
   body: string;
 }
 
+interface Comment {
+  postId: number,
+  id: number,
+  name: string,
+  email: string,
+  body: string;
+}
+
 export async function loader({ params }: { params?: Params }) {
   if (!params || !params.postId) {
     throw new Error("Invalid params or id");
@@ -19,11 +27,16 @@ export async function loader({ params }: { params?: Params }) {
   );
   const post = (await response.json()) as Post;
 
-  return { post };
+  const commentResponse = await fetch(`https://jsonplaceholder.typicode.com/posts/${params.postId}/comments`)
+  const comment = await commentResponse.json() as Comment[];
+
+  return { post, comment };
 }
 
 const Post = () => {
   const { post } = useLoaderData() as Awaited<ReturnType<typeof loader>>;
+  const { comment } = useLoaderData() as Awaited<ReturnType<typeof loader>>;
+
 
   return (
     <>
@@ -33,6 +46,15 @@ const Post = () => {
         <Text>{post.body}</Text>
       </CardBody>
     </Card>
+    <Text fontSize={36} align='center' fontWeight="bold">Comments</Text>
+    {comment.map(comment => (
+        <Card justify='center' align='center' key={comment.id} my={3}>
+          <CardHeader>{comment.name}</CardHeader>
+          <CardBody>
+            <Text>{comment.body}</Text>
+          </CardBody>
+        </Card>
+      ))}
     </>
   );
 };
